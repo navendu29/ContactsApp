907e43e23a43d085d3104561eb1080d2c5ade32c
navendu29 <navendugupta29@gmail.com>
2018-02-26 22:49:26 +0530

    CVS zipped

--- app/src/main/java/com/example/navendu/savecontacts/MainActivity.java
+++ app/src/main/java/com/example/navendu/savecontacts/MainActivity.java
@@ -1,42 +1,51 @@
-package com.example.navendu.savecontacts;
 
-import android.Manifest;
-import android.content.ContentResolver;
-import android.content.Context;
-import android.content.CursorLoader;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+        package com.example.navendu.savecontacts;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URI;
+        import android.Manifest;
+        import android.content.ContentResolver;
+        import android.content.Context;
+        import android.content.CursorLoader;
+        import android.content.DialogInterface;
+        import android.content.Intent;
+        import android.content.pm.PackageManager;
+        import android.database.Cursor;
+        import android.net.Uri;
+        import android.os.Build;
+        import android.os.Environment;
+        import android.provider.ContactsContract;
+        import android.support.annotation.NonNull;
+        import android.support.v4.app.ActivityCompat;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.Button;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.zip.ZipOutputStream;
+        import java.io.BufferedInputStream;
+        import java.io.BufferedOutputStream;
+        import java.io.File;
+        import java.io.FileInputStream;
+        import java.io.FileNotFoundException;
+        import java.io.FileOutputStream;
+        import java.io.IOException;
+        import java.net.URI;
 
-import de.siegmar.fastcsv.writer.CsvWriter;
+        import java.nio.charset.StandardCharsets;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
+        import java.util.zip.ZipEntry;
+        import java.util.zip.ZipOutputStream;
+
+        import de.siegmar.fastcsv.writer.CsvWriter;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 124;
+    String TAG = "MainActivity";
     TextView t1;
     Button b1;
     @Override
@@ -85,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-            File file=new File(Dir,"message.txt");
+            File file=new File(Dir,"message.csv");
             try {
 
 
@@ -100,6 +109,17 @@ public class MainActivity extends AppCompatActivity {
 
                 Log.i("THISSS", "fetchContacts:file stream success dcjdbjvjdvvjd");
 
+                String[] s = new String[1];
+                String inputPath = root.getAbsolutePath()+"/MYAPPNAVENDU";
+                // Type the path of the files in here
+                s[0] = inputPath + "/message.csv";
+//                s[1] = inputPath + "/textfile.txt"; // /sdcard/ZipDemo/textfile.txt
+
+                // first parameter is d files second parameter is zip file name
+//                ZipManager zipManager = new ZipManager();
+
+                // calling the zip function
+                zip(s, inputPath + "/contacts.zip");
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -112,50 +132,142 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"not available",Toast.LENGTH_SHORT).show();
         }
     }
+    private boolean doesUserHavePermission(Context context)
+    {
+        int result1 = context.checkCallingOrSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int result2 = context.checkCallingOrSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+        int result3 = context.checkCallingOrSelfPermission(Manifest.permission.READ_CONTACTS);
 
-    @Override
-    public void onRequestPermissionsResult(int requestcode, @NonNull String[] permission,@NonNull int[] granted) {
-        super.onRequestPermissionsResult(requestcode,permission,granted);
 
-        if(requestcode==1334)
-        {
+        return (result1 == PackageManager.PERMISSION_GRANTED) && (result2 == PackageManager.PERMISSION_GRANTED) &&(result3 == PackageManager.PERMISSION_GRANTED);
+    }
+    public void zip(String[] _files, String zipFileName) {
+        int BUFFER=1024;
+        try {
+            BufferedInputStream origin = null;
+            FileOutputStream dest = new FileOutputStream(zipFileName);
+            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(
+                    dest));
+            byte data[] = new byte[BUFFER];
 
-            if(permission.length>0)
-            {
-                if(granted[0]==PackageManager.PERMISSION_GRANTED)
-                {
-                    fetchContacts();
+            for (int i = 0; i < _files.length; i++) {
+                Log.v("Compress", "Adding: " + _files[i]);
+                Log.v("Compress","Destination"+zipFileName);
+                FileInputStream fi = new FileInputStream(_files[i]);
+                origin = new BufferedInputStream(fi, BUFFER);
 
+                ZipEntry entry = new ZipEntry(_files[i].substring(_files[i].lastIndexOf("/") + 1));
+                out.putNextEntry(entry);
+                int count;
+
+                while ((count = origin.read(data, 0, BUFFER)) != -1) {
+                    out.write(data, 0, count);
                 }
-                else if(granted[0]==PackageManager.PERMISSION_DENIED)
-                {
-                    Toast.makeText(getApplicationContext(),"please allow permission to save contacts",Toast.LENGTH_SHORT).show();
-                }
-
+                origin.close();
             }
-            else
-            {
-                Toast.makeText(getApplicationContext(),"please allow permission to save contacts",Toast.LENGTH_SHORT).show();
 
-            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else
-        {
-            Toast.makeText(getApplicationContext(),"please allow permission to save contacts",Toast.LENGTH_SHORT).show();
-
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
+            {
+                Map<String, Integer> perms = new HashMap<String, Integer>();
+                // Initial
+                perms.put(Manifest.permission.READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.READ_CONTACTS, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for ACCESS_FINE_LOCATION
+                if (perms.get(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    // All Permissions Granted
+                    fetchContacts();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "Some Permission is Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+            break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 
     public void hello(View view)
     {
+        if(!doesUserHavePermission(this))
+            permissionRequest();
+        else
+            fetchContacts();
 
+//        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1334);
+////        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1334);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 786);
+//        }
 
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1334);
-
+//        int REQUEST_WRITE_PERMISSION = 786;
 
 
     }
 
+    private void permissionRequest() {
+        List<String> permissionsNeeded = new ArrayList<String>();
 
+        final List<String> permissionsList = new ArrayList<String>();
+        if (!addPermission(permissionsList, Manifest.permission.READ_CONTACTS))
+            permissionsNeeded.add("Read Contacts");
+        if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            permissionsNeeded.add("Write External Storage");
+        if (!addPermission(permissionsList, Manifest.permission.READ_EXTERNAL_STORAGE))
+            permissionsNeeded.add("Write External Storage");
 
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Need Rationale
+                String message = "You need to grant access to " + permissionsNeeded.get(0);
+                for (int i = 1; i < permissionsNeeded.size(); i++)
+                    message = message + ", " + permissionsNeeded.get(i);
+//                showMessageOKCancel(message,
+//                        new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+//                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+//                            }
+//                        });
+//                return;
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                requestPermissions(permissionsList.toArray(new String[permissionsList.size()]),
+                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
+            }
+            return;
+        }
+
+//        insertDummyContact();
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsList.add(permission);
+                // Check for Rationale Option
+                if (!shouldShowRequestPermissionRationale(permission))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
